d6dfc4e093bd6c293ab241434fd352cb51a66ea3
Justin Balthrop <git@justinbalthrop.com>
2010-05-20 22:19:48 -0700

    simple tasks with hooks using dependency-based programming

---
+++ examples/fowler.clj
@@ -0,0 +1,11 @@
+(deftask code-gen []
+  (println "do the code generation"))
+
+(deftask compile [] code-gen
+  (println "do the compilation"))
+
+(deftask data-load [] code-gen
+  (println "load the test data"))
+
+(deftask test [] compile data-load
+  (println "run the tests"))
---
+++ src/cake.clj
@@ -0,0 +1,36 @@
+(ns cake)
+
+(def tasks (atom {}))
+
+(defn update [m k f & args]
+  (assoc m k (apply f (get m k) args)))
+
+(defn add-form [task form]
+  (let [task (or task {:actions [] :deps []})]
+    (cond
+     (symbol? form) (update task :deps    conj form)
+     (string? form) (update task :doc     str  form)
+     (list?   form) (update task :actions conj form)
+     :else (throw (IllegalArgumentException.
+                   "deftask requires forms to be a symbol, string or list")))))
+
+(defmacro deftask
+  "Define a cake task. Forms can be any of the following:
+   string - documentation to append to the task definition
+   symbol - the name of another task that is a prerequisite for this task
+   list   - code to be run when the task is executed"
+  [name params & forms]
+  `(swap! tasks update '~name (partial reduce add-form) '~forms))
+
+(defn run-task
+  ([name] (swap! tasks run-task name))
+  ([tasks name]
+     (let [task (tasks name)]
+       (if (:results task)
+         tasks
+         (let [tasks (reduce run-task tasks (task :deps))]
+           (assoc-in tasks [name :results]
+             (doall (map eval (task :actions)))))))))
+
+(defmacro dotask [name]
+  `(get-in (run-task '~name) ['~name :results]))
