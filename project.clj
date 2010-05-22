fc36087e4acda69db81dbad5f6409a74d1eb51c8
Justin Balthrop <git@justinbalthrop.com>
2010-05-22 16:07:30 -0700

    project.clj

---
+++ project.clj
@@ -0,0 +1,6 @@
+(defproject cake "0.1.0-SNAPSHOT"
+  :description "Save your fork, there's cake!"
+  :dependencies [[clojure "1.2.0-master-SNAPSHOT"]
+                 [clojure-contrib "1.2.0-SNAPSHOT"]
+                 [clojure-useful "0.1.0-SNAPSHOT"]])
+
