bc66bb51df256a17d71324faef2e4910d74539fa
lance bradley <l@ncebradley.org>
2010-05-26 18:04:51 -0700

    flesh out example

---
+++ examples/fowler/project.clj
@@ -0,0 +1,7 @@
+(defproject fowler "0.1.0-SNAPSHOT"
+  :description "cake example project"
+  :dependencies [[clojure "1.2.0-master-SNAPSHOT"]
+                 [clojure-contrib "1.2.0-SNAPSHOT"]
+                 [clojure-useful "0.1.1-SNAPSHOT"]
+                 [leiningen "1.2.0-SNAPSHOT"]])
+
