edd5688bdbace8839d12cedaf8201b961f55ec89
lance bradley <l@ncebradley.org>
2010-05-27 22:54:46 -0700

    enable verbose

--- examples/war/build.clj
+++ examples/war/build.clj
@@ -1,13 +1,17 @@
 (deftask compile
-  (println (seq (.getURLs (java.lang.ClassLoader/getSystemClassLoader))))
+  ;; (println (seq (.getURLs (java.lang.ClassLoader/getSystemClassLoader))))
   (println "compiling...")
   ;; (println (ns-name *ns*))
   (let [root       (:root project)
         classpath  (for [dir ["/src" "/lib*"]] (str root dir))]
     ;; (println classpath)
-    (cake.ant/ant org.apache.tools.ant.taskdefs.Javac {:destdir (java.io.File. root "classes")
-                                                       :fork true
-                                                       :classpath (org.apache.tools.ant.types.Path. @cake.ant/ant-project (apply str (interpose ":" classpath)))
-                                                       :srcdir (org.apache.tools.ant.types.Path. @cake.ant/ant-project (java.io.File. root "src"))
-                                                       :includes "*.clj"} false)))
+    (cake.ant/ant org.apache.tools.ant.taskdefs.Javac
+                  {:destdir (java.io.File. root "classes")
+                   :fork true
+                   :classpath (org.apache.tools.ant.types.Path. @cake.ant/ant-project (apply str (interpose ":" classpath)))
+                   :srcdir (org.apache.tools.ant.types.Path. @cake.ant/ant-project (java.io.File. root "src"))
+                   :includes "*.clj"
+                   :debug true
+                   :verbose true}
+                  false)))
 
