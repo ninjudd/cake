c87933d0c796ab7d0c1e9b64978c50bcfc29303a
lance bradley <l@ncebradley.org>
2010-05-27 22:43:48 -0700

    use interop for Path

--- examples/war/build.clj
+++ examples/war/build.clj
@@ -6,8 +6,8 @@
         classpath  (for [dir ["/src" "/lib*"]] (str root dir))]
     ;; (println classpath)
     (cake.ant/ant org.apache.tools.ant.taskdefs.Javac {:destdir (java.io.File. root "classes")
-                :fork true
-                :classpath (org.apache.tools.ant.types.Path. @cake.ant/ant-project (apply str (interpose ":" classpath)))
-                :srcdir (org.apache.tools.ant.types.Path. @cake.ant/ant-project (java.io.File. root "src"))
-                :includes "*.clj"})))
+                                                       :fork true
+                                                       :classpath (org.apache.tools.ant.types.Path. @cake.ant/ant-project (apply str (interpose ":" classpath)))
+                                                       :srcdir (org.apache.tools.ant.types.Path. @cake.ant/ant-project (java.io.File. root "src"))
+                                                       :includes "*.clj"} false)))
 
