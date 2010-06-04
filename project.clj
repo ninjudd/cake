9564cedcbe2ddd047c8256bc0c47ec73a0de3fc2
Justin Balthrop <git@justinbalthrop.com>
2010-06-03 22:51:36 -0700

    only compile namespaces that are stale

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject cake "0.2.4-SNAPSHOT"
+(defproject cake "0.2.4-SNAPSHOT" :aot :all
   :description "Save your fork, there's cake!"
   :dependencies [[org.clojure/clojure "1.2.0-master-SNAPSHOT"]
                  [org.clojure/clojure-contrib "1.2.0-SNAPSHOT"]
--- src/cake/tasks/compile.clj
+++ src/cake/tasks/compile.clj
@@ -1,6 +1,7 @@
 (ns cake.tasks.compile
   (:use cake cake.ant
-        [clojure.contrib.find-namespaces :only [find-namespaces-in-dir]])
+        [clojure.useful :only [include?]]
+        [clojure.contrib.find-namespaces :only [find-clojure-sources-in-dir read-file-ns-decl]])
   (:import [org.apache.tools.ant.taskdefs Javac Java]
            [java.io File]))
 
@@ -14,23 +15,38 @@
                 :failonerror       true
                 :includeantruntime false})))
 
-(defn to-compile [project]
-  (let [aot (or (:aot project) (:namespaces project))
-        aot (cond (string? aot) (vector aot)
-                  (= :all aot)  (find-namespaces-in-dir (File. (:source-path project)))
-                  :else         aot)]
-    (if-let [main (:main project)]
-      (conj aot main)
-      aot)))
+(defn stale? [sourcefile classfile]
+  (> (.lastModified sourcefile) (.lastModified classfile)))
 
-(defn compile-clj [project]
+(defn aot [project]
+  (let [aot (or (:aot project) (:namespaces project))]
+    (if (= :all aot)
+      (constantly true)
+      (fn [namespace]
+        (or (= namespace (:main project))
+            (include? namespace aot))))))
+
+(defn classfile [namespace]
+  (str (.. (str namespace) (replace "-" "_") (replace "." "/"))
+       "__init.class"))
+
+(defn stale-namespaces [project]
+  (let [compile? (aot project)]
+    (remove nil?
+      (for [sourcefile (find-clojure-sources-in-dir (File. (:source-path project)))]
+        (let [namespace  (second (read-file-ns-decl sourcefile))
+              classfile (File. (:compile-path project) (classfile namespace))]
+          (when (and (compile? namespace) (stale? sourcefile classfile))
+            namespace))))))
+
+(defn compile-clojure [project]
   (ant Java {:classname   "clojure.lang.Compile"
              :classpath   (classpath project)
              :fork        true
              :failonerror true}
        (sys {:clojure.compile.path (:compile-path project)})
-       (args (map name (to-compile project)))))
+       (args (map name (stale-namespaces project)))))
 
 (deftask compile
   (compile-java project)
-  (compile-clj project))
+  (compile-clojure project))
