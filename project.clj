fd3c47dee05f617efff5f2cf484ba148e497f7b9
Justin Balthrop <git@justinbalthrop.com>
2010-06-17 17:43:37 -0700

    remove jline

--- project.clj
+++ project.clj
@@ -2,7 +2,6 @@
   :description "Save your fork, there's cake!"
   :dependencies [[clojure "1.2.0-master-SNAPSHOT"]
                  [clojure-useful "0.2.1"]
-                 [jline "0.9.94"]
                  [org.apache.ant/ant "1.8.1"]
                  [org.apache.ant/ant-jsch "1.8.1"]
                  [org.clojars.ninjudd/maven-ant-tasks "2.1.0" :exclusions [ant/ant]]
