acc233b8b2d5683414c6aada4e631a5ce9f8b584
Justin Balthrop <git@justinbalthrop.com>
2010-06-29 15:47:05 -0700

    bump version

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject cake "0.2.8"
+(defproject cake "0.2.9"
   :description "Save your fork, there's cake!"
   :dependencies [[clojure "1.2.0-master-SNAPSHOT"]
                  [org.apache.ant/ant "1.8.1"]
