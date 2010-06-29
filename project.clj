18e690a89d0a1b764a729041ec76b1f22934261c
Justin Balthrop <git@justinbalthrop.com>
2010-06-29 15:54:54 -0700

    move ordered-set out of dev-dependencies

--- project.clj
+++ project.clj
@@ -1,10 +1,10 @@
 (defproject cake "0.2.9"
   :description "Save your fork, there's cake!"
   :dependencies [[clojure "1.2.0-master-SNAPSHOT"]
+                 [ordered-set "0.1.0"]
                  [org.apache.ant/ant "1.8.1"]
                  [org.apache.ant/ant-jsch "1.8.1"]
                  [org.clojars.ninjudd/maven-ant-tasks "2.1.0" :exclusions [ant/ant]]
                  [org.apache.maven.plugins/maven-shade-plugin "1.3.3"]]
   :dev-dependencies [[clojure-complete "0.1.0" :exclusions [clojure]]
-                     [clojure-useful   "0.2.3" :exclusions [clojure]]
-                     [ordered-set      "0.1.0" :exclusions [clojure]]])
+                     [clojure-useful   "0.2.3" :exclusions [clojure]]])
