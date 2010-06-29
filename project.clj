69f451919b67a7db65d209d865a39660c8d8545f
Justin Balthrop <git@justinbalthrop.com>
2010-06-29 11:42:28 -0700

    update useful

--- project.clj
+++ project.clj
@@ -6,5 +6,5 @@
                  [org.clojars.ninjudd/maven-ant-tasks "2.1.0" :exclusions [ant/ant]]
                  [org.apache.maven.plugins/maven-shade-plugin "1.3.3"]]
   :dev-dependencies [[clojure-complete "0.1.0" :exclusions [clojure]]
-                     [clojure-useful   "0.2.2" :exclusions [clojure]]
+                     [clojure-useful   "0.2.3" :exclusions [clojure]]
                      [ordered-set      "0.1.0" :exclusions [clojure]]])
