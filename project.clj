85664db66495999f89a35f671946903120a1c753
Justin Balthrop <git@justinbalthrop.com>
2010-07-19 12:54:48 -0700

    update to clojure 1.2 beta1

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject cake "0.3.1"
   :description "Save your fork, there's cake!"
-  :dependencies [[clojure "1.2.0-master-SNAPSHOT"]
+  :dependencies [[clojure "1.2.0-beta1"]
                  [ordered-set "0.1.0"]
                  [org.apache.ant/ant "1.8.1"]
                  [org.apache.ant/ant-jsch "1.8.1"]
