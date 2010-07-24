2ef2d2c7f544f2a0c5bb9d81eccd822f1538ee2e
lance bradley <l@ncebradley.org>
2010-07-23 19:45:39 -0700

    add core test file

---
+++ test/test_core.clj
@@ -0,0 +1,32 @@
+(ns test-core
+  (:use clojure.test cake))
+
+(defmacro project-let [bindings & body]
+  `(binding [project {:name "project-stub", :version "0.0.0", :root "/home/project"}]
+     (let ~bindings
+       ~@body)))
+
+(deftest file-fn
+  (testing "single string"
+    (project-let [s "foo/bar/baz"]
+      (is (= (str (:root project) "/" s)
+             (.toString (file s))))))
+  
+  (testing "multiple strings"
+    (project-let [a "foo", b "bar"]
+      (is (= (str (:root project) "/" a "/" b)
+             (.toString (file a b))))))
+
+  ;; working here
+  (comment testing "single file"
+    (project-let [s "foo", f (java.io.File. s)]
+      (is (= (str (:root project) "/" s)
+             (.toString (file f))))))
+  
+  (testing "file and string")
+  
+  (testing "tilde expansion")
+
+  (testing "home directory"))
+
+
