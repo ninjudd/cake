9ba263b25a4099c3711259975062e2b9be8b498d
lance bradley <l@ncebradley.org>
2010-07-23 19:43:26 -0700

    test --auto prints . on all tests passing

--- bake/bake/test.clj
+++ bake/bake/test.clj
@@ -31,6 +31,37 @@
        namespaces
        (map prep-opt tests)))))
 
+(defonce auto-count (atom 0))
+
+(defmethod clojure.test/report :begin-test-ns [m]
+  (clojure.test/with-test-out
+   (println "\nTesting" (ns-name (:ns m)))))
+
+(defmethod clojure.test/report :summary [m]
+  (clojure.test/with-test-out
+   (println "\nRan" (:test m) "tests containing"
+            (+ (:pass m) (:fail m) (:error m)) "assertions.")
+   (println (:fail m) "failures," (:error m) "errors.")))
+
+(defmethod clojure.test/report :begin-auto [m])
+
+(defmethod clojure.test/report :summary-auto [m]
+  (swap! auto-count inc)
+  (clojure.test/with-test-out
+    (if (and (= 0 (:fail m))
+             (= 0 (:error m)))
+      (println ".")
+      (do
+        (println "\nRan" (:test m) "tests containing"
+                 (+ (:pass m) (:fail m) (:error m)) "assertions.")
+        (println (:fail m) "failures," (:error m) "errors.")))))
+
+(declare start-time)
+
+(defn timer [begin]
+  (println "----")
+  (println "Finished in" (/ (- (System/nanoTime) begin) (Math/pow 10 9)) "seconds.\n"))
+
 (defn run-tests-for-fns [grouped-tests]
   (when-let [input-fs (:fn grouped-tests)]
     (println "testing functions:" (apply str (interpose ", " input-fs)))
@@ -40,21 +71,32 @@
 
 (defn run-tests-for-nses [grouped-tests]
   (for [ns (:ns grouped-tests)]
-    (binding [clojure.test/*report-counters* (ref clojure.test/*initial-report-counters*)]
+    (binding [clojure.test/*report-counters* (ref clojure.test/*initial-report-counters*)
+              start-time (System/nanoTime)]
       (clojure.test/report {:type :begin-test-ns :ns ns})
       (clojure.test/test-all-vars ns)
       (clojure.test/report (assoc @clojure.test/*report-counters* :type :summary))
-      (println "----")
+      (timer start-time)
       @clojure.test/*report-counters*)))
 
 (defn run-tests-for-tags [grouped-tests test-namespaces]
   (when-let [input-tags (:tag grouped-tests)]
     (let [tags-to-fs (map-tags test-namespaces)]
       (doall (for [tag input-tags]
-               (binding [clojure.test/*report-counters* (ref clojure.test/*initial-report-counters*)]
+               (binding [clojure.test/*report-counters* (ref clojure.test/*initial-report-counters*)
+                         start-time (System/nanoTime)]
                  (println "Testing" tag)
                  (doseq [test (tag tags-to-fs)]
                    (clojure.test/test-var test))
                  (clojure.test/report (assoc @clojure.test/*report-counters* :type :summary))
-                 (println "----" )
+                 (timer start-time)
                  @clojure.test/*report-counters*))))))
+
+(defn run-tests-for-auto [grouped-tests]
+  (for [ns (:ns grouped-tests)]
+    (binding [clojure.test/*report-counters* (ref clojure.test/*initial-report-counters*)
+              start-time (System/nanoTime)]
+      (clojure.test/report {:type :begin-auto :ns ns})
+      (clojure.test/test-all-vars ns)
+      (clojure.test/report (assoc @clojure.test/*report-counters* :type :summary-auto))
+      @clojure.test/*report-counters*)))
--- src/cake/tasks/test.clj
+++ src/cake/tasks/test.clj
@@ -5,15 +5,15 @@
 (deftask test #{compile}
   "Run project tests."
   (bake (:use bake.test) []
-    (let [start (System/nanoTime)
-          nses  (all-test-namespaces project)]
+    (let [nses  (all-test-namespaces project)]
       (doseq [ns nses] (require ns))
       (binding [clojure.test/*test-out* *out*]
         (let [grouped-tests (get-grouped-tests nses opts)]
-          (let [results (concat
-                         (run-tests-for-fns  grouped-tests)
-                         (run-tests-for-nses grouped-tests)
-                         (run-tests-for-tags grouped-tests (all-test-namespaces project)))]
+          (let [results (if (:auto opts)
+                          (run-tests-for-auto grouped-tests)
+                          (concat
+                           (run-tests-for-fns  grouped-tests)
+                           (run-tests-for-nses grouped-tests)
+                           (run-tests-for-tags grouped-tests (all-test-namespaces project))))]
             (if (> (count results) 1)
-              (-> (apply merge-with + results) (assoc :type :summary) clojure.test/report)))))
-      (println "Finished in" (/ (- (System/nanoTime) start) (Math/pow 10 9)) "seconds.\n"))))
+              (-> (apply merge-with + results) (assoc :type :summary) clojure.test/report))))))))
