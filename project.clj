3cc56f265e2ec6e7cd7e6e0337bb96a5cee9a94c
Justin Balthrop <git@justinbalthrop.com>
2010-07-25 11:10:20 -0700

    update clojure-useful and clojure-complete to fix 1.1 error

--- project.clj
+++ project.clj
@@ -6,5 +6,5 @@
                  [org.apache.ant/ant-jsch "1.8.1"]
                  [org.clojars.ninjudd/maven-ant-tasks "2.1.0" :exclusions [ant/ant]]
                  [org.apache.maven.plugins/maven-shade-plugin "1.3.3"]]
-  :dev-dependencies [[clojure-complete "0.1.0" :exclusions [clojure]]
-                     [clojure-useful   "0.2.3" :exclusions [clojure]]])
+  :dev-dependencies [[clojure-complete "0.1.1" :exclusions [clojure]]
+                     [clojure-useful   "0.2.4" :exclusions [clojure]]])
