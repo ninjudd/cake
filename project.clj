3de17335a9dd769ca556e261fadcbdde19b10858
Justin Balthrop <git@justinbalthrop.com>
2010-08-02 14:11:05 -0700

    remove difform dep for now

--- project.clj
+++ project.clj
@@ -5,7 +5,6 @@
                  [org.apache.ant/ant "1.8.1"]
                  [org.apache.ant/ant-jsch "1.8.1"]
                  [org.clojars.ninjudd/maven-ant-tasks "2.1.0" :exclusions [ant/ant]]
-                 [org.apache.maven.plugins/maven-shade-plugin "1.3.3"]
-                 [org.clojars.lancepantz/difform "1.0.2"]]
+                 [org.apache.maven.plugins/maven-shade-plugin "1.3.3"]]
   :dev-dependencies [[clojure-complete "0.1.2" :exclusions [clojure]]
                      [clojure-useful   "0.2.5" :exclusions [clojure]]])
