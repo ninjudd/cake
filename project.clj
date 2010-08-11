51969f76c61ad277e9f041cb4c64fcc18775aaec
Justin Balthrop <git@justinbalthrop.com>
2010-08-11 12:30:49 -0700

    fix kill and ps in windows

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject cake "0.3.9-SNAPSHOT"
+(defproject cake "0.3.11"
   :description "Save your fork, there's cake!"
   :dependencies [[clojure "1.2.0-RC2"]
                  [ordered-set "0.1.0"]
