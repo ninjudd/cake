74d5bcd3e3d34cf8c17492814c857d781035de37
Justin Balthrop <git@justinbalthrop.com>
2010-08-23 18:26:15 -0700

    support test filters in autotest

--- src/cake/tasks/test.clj
+++ src/cake/tasks/test.clj
@@ -2,20 +2,23 @@
   (:use cake cake.core)
   (:import [java.io File]))
 
-(defn test-opts [args]
-  (if (empty? args)
-    {:all true}
-    (group-by #(cond (keyword?  %) :tags
-                     (namespace %) :functions
-                     :else         :namespaces)
-              (map read-string args))))
+(defn test-opts []
+  (let [args (into (:test *opts*) (:autotest *opts*))]
+    (if (empty? args)
+      {:all true}
+      (group-by #(cond (keyword?  %) :tags
+                       (namespace %) :functions
+                       :else         :namespaces)
+                (map read-string args)))))
 
 (deftask test #{compile}
   "Run project tests."
-  (bake (:use bake.test) [opts (test-opts (:test *opts*))]
+  "Specify which tests to run as arguments like: namespace, namespace/function, or :tag"
+  (bake (:use bake.test) [opts (test-opts)]
     (run-project-tests opts)))
 
 (deftask autotest #{compile}
   "Automatically run tests whenever your project code changes."
-  (bake (:use bake.test) [opts (test-opts (:autotest *opts*))]
+  "Specify tests to run just like the test task. Specify the interval with --interval."
+  (bake (:use bake.test) [opts (test-opts)]
     (run-project-tests (assoc opts :quiet true))))
