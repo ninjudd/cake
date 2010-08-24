8d85ba30a2e118a1c41c8be3f80b0d6be8122448
Justin Balthrop <git@justinbalthrop.com>
2010-08-23 18:10:05 -0700

    fix autotest

--- bake/bake/test.clj
+++ bake/bake/test.clj
@@ -1,9 +1,7 @@
 (ns bake.test
-  (:refer-clojure :exclude [group-by])
-  (:use [cake.contrib.find-namespaces :only [find-namespaces-in-dir]]
-        useful)
-  (:require [clojure.test :as test]
-            [clojure.stacktrace :as stack]))
+  (:use clojure.test
+        [cake.contrib.find-namespaces :only [find-namespaces-in-dir]])
+  (:import [java.io StringWriter]))
 
 (defn test-pred [opts]
   (let [tags       (set (opts :tags))
@@ -11,28 +9,31 @@
         namespaces (set (opts :namespaces))]
     (fn [ns [name f]]
       (and (:test (meta f))
-           (or (empty? opts)
+           (or (:all opts)
                (namespaces ns)
-               (some tags (:tags (meta f)))
-               (tags (:tag (meta f)))
+               (some tags (:tags (meta f))) (tags (:tag (meta f)))
                (functions (symbol (str ns "/" name))))))))
 
-(defn run-tests [opts]
+(defn run-project-tests [opts]
   (let [start (System/nanoTime)
         run?  (test-pred opts)]
-    (binding [test/*test-out* *out*]
-      (doseq [ns (find-namespaces-in-dir (java.io.File. "test"))]
-        (require ns)
-        (when-let [tests (seq (filter (partial run? ns) (ns-publics ns)))]
-          (let [ns-meta (meta (find-ns ns))
-                once-fixtures (test/join-fixtures (::test/once-fixtures ns-meta))
-                each-fixtures (test/join-fixtures (::test/each-fixtures ns-meta))]
-            (binding [test/*report-counters* (ref test/*initial-report-counters*)]
-              (test/report {:type :begin-test-ns :ns ns})
-              (once-fixtures
-               (fn []
-                 (doseq [[name f] tests]
-                   (each-fixtures #(test/test-var f)))))
-              (test/report (assoc @test/*report-counters* :type :summary)))))))
-    (println "----")
-    (println "Finished in" (/ (- (System/nanoTime) start) (Math/pow 10 9)) "seconds.\n")))
+    (doseq [ns (find-namespaces-in-dir (java.io.File. "test"))]
+      (require ns)
+      (when-let [tests (seq (filter (partial run? ns) (ns-publics ns)))]
+        (let [ns-meta (meta (find-ns ns))
+              once-fixtures (join-fixtures (:clojure.test/once-fixtures ns-meta))
+              each-fixtures (join-fixtures (:clojure.test/each-fixtures ns-meta))]
+          (binding [*test-out* (StringWriter.)
+                    *report-counters* (ref *initial-report-counters*)]
+            (report {:type :begin-test-ns :ns ns})
+            (once-fixtures
+             (fn []
+               (doseq [[name f] tests]
+                 (each-fixtures #(test-var f)))))
+            (report (assoc @*report-counters* :type :summary))
+            (when (or (not (:quiet opts)) (< 0 (apply + (map @*report-counters* [:fail :error]))))
+              (print (.toString *test-out*))
+              (flush))))))
+    (when-not (:quiet opts)
+      (println "----")
+      (println "Finished in" (/ (- (System/nanoTime) start) (Math/pow 10 9)) "seconds.\n"))))
--- src/cake/tasks/test.clj
+++ src/cake/tasks/test.clj
@@ -2,16 +2,20 @@
   (:use cake cake.core)
   (:import [java.io File]))
 
-(defn test-opts []
-  (group-by
-   #(cond (keyword?  %) :tags
-          (namespace %) :functions
-          :else         :namespaces)
-   (map read-string (:test *opts*))))
+(defn test-opts [args]
+  (if (empty? args)
+    {:all true}
+    (group-by #(cond (keyword?  %) :tags
+                     (namespace %) :functions
+                     :else         :namespaces)
+              (map read-string args))))
 
 (deftask test #{compile}
   "Run project tests."
-  (bake (:use bake.test) [opts (test-opts)]
-    (run-tests opts)))
+  (bake (:use bake.test) [opts (test-opts (:test *opts*))]
+    (run-project-tests opts)))
 
-(deftask autotest "Automatically run tests whenever your project code changes.")
+(deftask autotest #{compile}
+  "Automatically run tests whenever your project code changes."
+  (bake (:use bake.test) [opts (test-opts (:autotest *opts*))]
+    (run-project-tests (assoc opts :quiet true))))
