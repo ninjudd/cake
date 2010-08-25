b4f86600bab9bf5c23117925c12e921d0622ce99
Justin Balthrop <git@justinbalthrop.com>
2010-08-25 15:23:20 -0700

    add clj-stacktrace to :dev-dependencies

--- project.clj
+++ project.clj
@@ -6,5 +6,6 @@
                  [org.apache.ant/ant "1.8.1"]
                  [org.clojars.ninjudd/maven-ant-tasks "2.1.0" :exclusions [ant/ant]]
                  [org.apache.maven.plugins/maven-shade-plugin "1.3.3"]]
-  :dev-dependencies [[clojure-complete "0.1.2" :exclusions [clojure]]
-                     [clojure-useful   "0.2.6" :exclusions [clojure]]])
+  :dev-dependencies [[clojure-complete "0.1.2"]
+                     [clojure-useful   "0.2.6"]
+                     [clj-stacktrace   "0.1.3"]])
