6b89cfa2a118c41ebb189d6352af308336847852
Justin Balthrop <git@justinbalthrop.com>
2010-08-29 08:48:54 -0700

    fix clean task to be more verbose and delete *.jar/war

--- src/cake/tasks/clean.clj
+++ src/cake/tasks/clean.clj
@@ -3,13 +3,19 @@
   (:import (org.apache.tools.ant.taskdefs Delete Mkdir)))
 
 (defn clean-dir [dir]
-  (ant Delete {:include-empty-dirs true}
-       (add-fileset {:dir dir :includes "**/*"})))
+  (when (seq (rest (file-seq dir)))
+    (log "Deleting" (str (.getPath dir) "/*"))
+    (ant Delete {:include-empty-dirs true}
+      (add-fileset {:dir dir :includes "**/*"}))))
 
 (deftask clean
   "Remove cake build artifacts."
+  (ant Delete {:verbose true}
+    (add-fileset {:dir (file) :includes "*.jar"})
+    (add-fileset {:dir (file) :includes "*.war"}))
   (doseq [dir ["classes" "build"]]
-    (clean-dir dir))
+    (clean-dir (file dir)))
   (when (= ["deps"] (:clean *opts*))
-    (clean-dir "lib")
-    (ant Delete {:file "pom.xml"})))
+    (clean-dir (file "lib"))
+    (ant Delete {:verbose}
+      (add-fileset {:dir (file) :includes "pom.xml"}))))
