da4bc2340398a52408b26bba2b644dd1b909d8ff
Justin Balthrop <git@justinbalthrop.com>
2010-08-30 18:40:42 -0700

    version 0.4.12-SNAPSHOT

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject cake "0.4.11"
+(defproject cake "0.4.12-SNAPSHOT"
   :description "Save your fork, there's cake!"
   :dependencies [[clojure "1.2.0"]
                  [ordered-set "0.1.0"]
