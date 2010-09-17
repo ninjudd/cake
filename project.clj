039b04de0ea074dc0b7ccc6a6edeaa73b5442557
ninjudd <git@justinbalthrop.com>
2010-09-17 15:55:40 -0700

    version 0.4.16-SNAPSHOT

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject cake "0.4.15"
+(defproject cake "0.4.16-SNAPSHOT"
   :description "Save your fork, there's cake!"
   :dependencies [[clojure "1.2.0"]
                  [ordered-set "0.1.0"]
