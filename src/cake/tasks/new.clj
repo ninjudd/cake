077a528e681d72e47235a507f4219bab00336825
Raynes <DiscipleRayne@gmail.com>
2010-09-28 18:01:06 -0500

    Cleanup based on recommendations by ninjudd.

--- src/cake/tasks/new.clj
+++ src/cake/tasks/new.clj
@@ -11,6 +11,16 @@
   :dependencies [[clojure \"1.2.0\"]])
 ")
 
+(def template-dir (file "~" ".cake" "template"))
+
+(defn rename-dirs [root project]
+  (log "Renaming directories with +project+ in their name")
+  (doseq [old-file (reverse (sort-by (comp count str) (file-seq root)))
+          :let [name (.getName old-file)
+                replaced (.replace name "+project+" project)]
+          :when (not= name replaced)]
+    (.renameTo old-file (file (.getParent old-file) replaced))))
+
 (defn template-new [project]
   (let [root (file *pwd* project)]
     (if (.exists root)
@@ -18,40 +28,32 @@
       (do
         (log "Creating a new project based on ~/.cake/template")
         (ant Copy {:todir root} (add-fileset {:dir (str (file "~" ".cake" "template"))}))
-        (let [files (file-seq root)
-              rep (filter identity
-                          (map #(let [name (.getName %)
-                                      replaced (.replace name "+project+" project)]
-                                  (when (not= replaced name) [% (file (.getParent %) replaced)]))
-                               files))]
-          (log "Renaming directories with +project+ in their name")
-          (doseq [[from to] (sort-by (comp count str second) #(or (and (= % %2) 0) 1) rep)]
-            (.renameTo from to))
-          (let [files (file-seq root)]
-            (log (str "Replacing +project+ with '" project "' in all files."))
-            (doseq [f (filter #(.isFile %) files)]
-              (spit f (.replace (slurp f) "+project+" project)))))))))
+        (rename-dirs root project)
+        (let [files (file-seq root)]
+          (log (str "Replacing +project+ with '" project "' in all files."))
+          (doseq [f (filter #(.isFile %) files)]
+            (spit f (.replace (slurp f) "+project+" project))))))))
+
+(defn create-default-template [project]
+  (do
+    (log "Creating template directory: ~/.cake/template")
+    (ant Mkdir {:dir template-dir})
+    (ant Mkdir {:dir (file template-dir "src" "+project+")})
+    (spit (file template-dir "src" "+project+" "core.clj") "(ns +project+.core)")
+    (log "Created template file: src/+project+/core.clj")
+    (ant Mkdir {:dir (file template-dir "test")})
+    (spit (file template-dir "project.clj") (format default-template project))
+    (log "Created template file: project.clj")
+    (spit (file template-dir ".gitignore")
+          (apply str (interleave [".cake" "pom.xml" "*.jar" "*.war" "lib" "classes" "build" project] (repeat "\n"))))
+    (log "Created template file: .gitignore")
+    (extract-resource "LICENSE" template-dir)
+    (log "Created template LICENSE file (Eclipse Public License)")
+    (template-new project)))
 
 (deftask new
   "Create scaffolding for a new project."
   "You can put a default project template in ~/.cake/template. Substitute +project+ anywhere
    that you want your project name to be."
   [{[project] :new}]
-  (let [template-dir (file "~" ".cake" "template")]
-    (if (.exists template-dir)
-      (template-new project)
-      (do
-        (log "Creating template directory: ~/.cake/template")
-        (ant Mkdir {:dir template-dir})
-        (ant Mkdir {:dir (file template-dir "src" "+project+")})
-        (spit (file template-dir "src" "+project+" "core.clj") "(ns +project+.core)")
-        (log "Created template file: src/+project+/core.clj")
-        (ant Mkdir {:dir (file template-dir "test")})
-        (spit (file template-dir "project.clj") (format default-template project))
-        (log "Created template file: project.clj")
-        (spit (file template-dir ".gitignore")
-              (apply str (interleave [".cake" "pom.xml" "*.jar" "*.war" "lib" "classes" "build" project] (repeat "\n"))))
-        (log "Created template file: .gitignore")
-        (extract-resource "LICENSE" template-dir)
-        (log "Created template LICENSE file (Eclipse Public License)")
-        (template-new project)))))
+  (if (.exists template-dir) (template-new project) (create-default-template project)))
