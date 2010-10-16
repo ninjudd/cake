e0a31e0f0599513f471751a110cc05717c2a8c9c
ninjudd <git@justinbalthrop.com>
2010-10-16 12:31:42 -0700

    switch back to private version of find-namespaces since i had to rewrite lazytest.tracker anyway

--- src/cake/tasks/compile.clj
+++ src/cake/tasks/compile.clj
@@ -1,7 +1,7 @@
 (ns cake.tasks.compile
   (:use cake cake.core cake.ant cake.file
         [cake.project :only [verbose? debug? log]]
-        [clojure.contrib.find-namespaces :only [find-clojure-sources-in-dir read-file-ns-decl]]
+        [cake.utils.find-namespaces :only [find-clojure-sources-in-dir read-file-ns-decl]]
         [cake.tasks.deps :only [os-name os-arch]]
         [cake.utils.useful :only [pluralize]])
   (:import [org.apache.tools.ant.taskdefs Copy Javac Java]))
