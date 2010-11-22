f2e9583175f646f9da3d7f73ac82f64aa6693f77
ninjudd <git@justinbalthrop.com>
2010-11-21 21:53:44 -0800

    pass Java core types directly in bake (so we can pass things like Sockets and InputStreams to the project classloader)

--- src/cake/project.clj
+++ src/cake/project.clj
@@ -9,7 +9,7 @@
 (def global-root (.getPath (File. (System/getProperty "user.home") ".cake")))
 
 (defn classpath []
-  (map #(str "file:" (.getPath %) (if (.isDirectory %) "/" " "))
+  (map #(str "file:" (.getPath %) (if (.isDirectory %) "/" ""))
        (concat (map file [(System/getProperty "bake.path")
                           "src/" "src/clj/" "classes/" "resources/" "dev/" "test/" "test/classes/"])
                (fileset-seq {:dir "lib"     :includes "*"})
@@ -40,32 +40,48 @@
        (conj v form)))
    [] bindings))
 
+(defn- separate-bindings
+  "Separate bindings based on whether their value is a Java core type or not, because Java types
+   should be passed directly to the project classloader, while other values should be serialized."
+  [bindings]
+  (reduce (fn [b [sym val]]
+            (if (.getClassLoader (class val))
+              (update b 0 conj  sym val)
+              (update b 1 assoc sym val)))
+          [[] {}]
+          (partition 2 bindings)))
+
+(defn- shared-bindings []
+  `[~'cake/*current-task* '~*current-task*
+    ~'cake/*project*      '~*project*
+    ~'cake/*context*      '~*context*
+    ~'cake/*script*       '~*script*
+    ~'cake/*opts*         '~*opts*
+    ~'cake/*pwd*          '~*pwd*
+    ~'cake/*env*          '~*env*
+    ~'cake/*vars*         '~*vars*])
+
 (defn project-eval [ns-forms bindings body]
-  (let [form
-        `(do (ns ~(symbol (str "bake.task." (name *current-task*)))
-               (:use ~'cake)
-               ~@ns-forms)
-             (let ~(quote-if odd? bindings)
-               ~@body))]
-    (eval-in
-     classloader
-     `(fn [ins# outs#]
-        (clojure.main/with-bindings
-          (bake.io/with-streams ins# outs#
-            (binding [~'cake/*current-task* '~*current-task*
-                      ~'cake/*project*      '~*project*
-                      ~'cake/*context*      '~*context*
-                      ~'cake/*script*       '~*script*
-                      ~'cake/*opts*         '~*opts*
-                      ~'cake/*pwd*          '~*pwd*
-                      ~'cake/*env*          '~*env*
-                      ~'cake/*vars*         '~*vars*]
-              (eval '~form)))))
-     *ins* *outs*)))
+  (let [[let-bindings object-bindings] (separate-bindings bindings)
+        temp-ns (gensym "bake")
+        form
+        `(do (binding [*ns* nil]
+               (ns ~temp-ns
+                 (:use ~'cake)
+                 ~@ns-forms))
+             (fn [ins# outs# ~@(keys object-bindings)]
+               (clojure.main/with-bindings
+                 (bake.io/with-streams ins# outs#
+                   (binding ~(shared-bindings)
+                     (let ~(quote-if odd? let-bindings)
+                       (in-ns '~temp-ns)
+                       (try ~@body
+                            (finally (remove-ns '~temp-ns)))))))))]
+    (apply eval-in classloader form *ins* *outs* (vals object-bindings))))
 
 (defmacro bake
   "Execute code in a your project classloader. Bindings allow passing state to the project
-   classloader. Namespace forms like use and require must be specified before bindings."  
+   classloader. Namespace forms like use and require must be specified before bindings."
   {:arglists '([ns-forms* bindings body*])}
   [& forms]
   (let [[ns-forms [bindings & body]] (split-with (complement vector?) forms)]
