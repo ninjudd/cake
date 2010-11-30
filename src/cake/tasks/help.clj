c55d42d6b044bbf6c5615c6d7ffae2d8f5c16fa5
ninjudd <git@justinbalthrop.com>
2010-11-30 11:55:06 -0800

    remove reload system task

--- src/cake/tasks/help.clj
+++ src/cake/tasks/help.clj
@@ -11,7 +11,7 @@
     (apply println "cake" name deps)
     (doseq [doc docs] (println "  " doc))))
 
-(def system-tasks #{"reload" "ps" "kill" "killall" "log" "upgrade"})
+(def system-tasks #{"ps" "kill" "killall" "log" "upgrade"})
 (def hidden-tasks #{"default" "eat" "bake"})
 
 (defn taskdocs [pattern]
