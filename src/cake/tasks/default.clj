0330934f0b56a1e7fd6f8b71e79f0fe772c3c208
marvinthepa <forke@uni-bonn.de>
2011-01-04 00:49:58 +0800

    added ng task to start nailgun server for vim
    shamelessly ripped of here:
    http://groups.google.com/group/clojure-cake/browse_thread/thread/da56f9c06e418450

--- src/cake/tasks/default.clj
+++ src/cake/tasks/default.clj
@@ -1,6 +1,6 @@
 (ns cake.tasks.default
   (:use cake.core))
 
-(require-tasks [cake.tasks help jar test compile deps release swank file version eval bake])
+(require-tasks [cake.tasks help jar test ng compile deps release swank file version eval bake])
 
 (deftask default #{help})
