706e36fb26bf548a50a75f31ad868ee1fd1b572e
David Santiago <david.santiago@gmail.com>
2011-02-10 17:12:53 -0600

    Updated check task so that it will not attempt to run on namespaces that depend on cake.core, avoiding a failure.

--- src/cake/tasks/check.clj
+++ src/cake/tasks/check.clj
@@ -5,17 +5,28 @@
         [cake.file :only [file rmdir]]
         [cake.tasks.compile :only [source-dir]]))
 
-(deftask check
+(deftask check #{compile-java}
   "Check syntax and warn on reflection."
   (let [source-path (source-dir)]
     (bake (:use [bake.core :only [log print-stacktrace]]
                 [bake.find-namespaces :only [find-clojure-sources-in-dir
-                                             read-file-ns-decl]])
+                                             read-file-ns-decl]]
+                [bake.nsdeps :only [deps-from-ns-decl]])
           [source-path source-path] ;; Just copy the var over to bake.
           (binding [*warn-on-reflection* true]
             (doseq [src-file (find-clojure-sources-in-dir source-path)]
-              (log "Compiling namespace " (second (read-file-ns-decl src-file)))
-              (try
-                (load-file (.getPath src-file))
-                (catch ExceptionInInitializerError e
-                  (print-stacktrace e))))))))
+              (let [depends-on-cake-core?
+                    (fn [ns-decl]
+                      (contains? (deps-from-ns-decl ns-decl) 'cake.core))
+                    src-file-ns-decl (read-file-ns-decl src-file)]
+                ;; Skip any namespaces that depend on cake.core, as they
+                ;; will fail.
+                (if (not (depends-on-cake-core? src-file-ns-decl))
+                  (do (log "Compiling namespace " (second src-file-ns-decl))
+                      (try
+                        (load-file (.getPath src-file))
+                        (catch ExceptionInInitializerError e
+                          (print-stacktrace e))))
+                  (do
+                    (log "Skipping namespace " (second src-file-ns-decl))
+                    (log "  Reason: Dependency on cake.core")))))))))
