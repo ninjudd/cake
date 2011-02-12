968caac7c6098e43cf611937cb89a1112016ce01
ninjudd <git@justinbalthrop.com>
2011-02-12 07:46:28 -0800

    version 0.6.1

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject cake "0.6.1-SNAPSHOT"
+(defproject cake "0.6.1"
   :description "Save your fork, there's cake!"
   :dependencies [[clojure "1.2.0"]
                  [clojure-contrib "1.2.0"]
