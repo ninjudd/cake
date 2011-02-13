c3eedd3d1dd6cb64cc221c1a0402290cfcc09cc0
David Santiago <david.santiago@gmail.com>
2011-02-13 03:18:23 -0600

    Updated check task to reload project before and after; this should fix problem which was causing unreliable results based on whether a class was already aot compiled or not. In particular, this was causing the check task to fail on clojure-protobuf and unreliable results when run on projects where certain classes were already aot-compiled.

--- src/cake/tasks/check.clj
+++ src/cake/tasks/check.clj
@@ -3,11 +3,13 @@
   (:use cake
         cake.core
         [cake.file :only [file rmdir]]
+        [cake.project :only [reload!]]
         [cake.tasks.compile :only [source-dir]]))
 
 (deftask check #{compile-java}
   "Check syntax and warn on reflection."
   (let [source-path (source-dir)]
+    (reload!)
     (bake (:use [bake.core :only [log print-stacktrace]]
                 [bake.find-namespaces :only [find-clojure-sources-in-dir
                                              read-file-ns-decl]]
@@ -29,4 +31,5 @@
                           (print-stacktrace e))))
                   (do
                     (log "Skipping namespace " (second src-file-ns-decl))
-                    (log "  Reason: Dependency on cake.core")))))))))
+                    (log "  Reason: Dependency on cake.core")))))))
+    (reload!)))
