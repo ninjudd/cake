0815c86c46b6d6320bd9ca71f21b3229f9132438
Anthony Grimes <i@raynes.me>
2011-06-23 18:50:22 -0500

    Renamed task-docs to gen-docs. Make output filename configurable. Log something interesting.

--- src/cake/tasks/docs.clj
+++ src/cake/tasks/docs.clj
@@ -1,17 +1,21 @@
 (ns cake.tasks.docs
   (:use cake
         [clojure.string :only [join]]
+        [bake.core :only [log]]
         (cake [core :only [deftask]]
               [task :only [tasks]])))
 
-(deftask task-docs #{deps}
+(deftask gen-docs #{deps}
   "Generate documentation for tasks."
   "If a filename is passed, documentation is written to that file. Otherwise,
 it is written to tasks.md in the current directory."
-  (spit
-   "tasks.md"
-   (join
-    (for [[k {:keys [docs deps]}] tasks]
-      (str "#### " k "\n\n"
-           (when (seq docs) (str (join "\n" docs) "\n\n"))
-           (when (seq deps) (str "dependencies: " deps "\n\n")))))))
+  {[file] :task-docs}
+  (let [file (or file "tasks.md")]
+    (log (str "Generating documentation in " file "."))
+    (spit
+     file
+     (join
+      (for [[k {:keys [docs deps]}] tasks]
+        (str "#### " k "\n\n"
+             (when (seq docs) (str (join "\n" docs) "\n\n"))
+             (when (seq deps) (str "dependencies: " deps "\n\n"))))))))
