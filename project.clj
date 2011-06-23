c5aa6e94cb7e1d62de5f0ce8136292746d5fe850
Justin Balthrop <git@justinbalthrop.com>
2011-06-22 19:15:48 -0700

    update uncle

--- project.clj
+++ project.clj
@@ -2,7 +2,7 @@
   :description "Save your fork, there's cake!"
   :dependencies [[clojure "1.2.0"]
                  [clojure-contrib "1.2.0"]
-                 [uncle "0.2.0"]
+                 [uncle "0.2.1"]
                  [classlojure "0.5.0"]
                  [clojure-useful "0.3.8"]
                  [com.jcraft/jsch "0.1.42"]
