19acd26c421a370b34a69435277d4d0809519d82
Alan Malloy <alan@malloys.org>
2011-07-05 12:21:33 -0700

    Let you know what % of your functions are tested

--- src/cake/tasks/coverage.clj
+++ src/cake/tasks/coverage.clj
@@ -39,19 +39,28 @@
 (defn used-symbols [file]
   (filter symbol? (flatten (read-all (java.io.PushbackReader. (reader file))))))
 
-(defn un-tested-symbols []
-  (let [targets (testable-symbols (testable-namespaces))
-        tested (set (mapcat used-symbols (clj-test-files)))
-        untested (remove (comp tested symbol name) targets)]
+(defn un-tested-symbols [testables]
+  (let [tested (set (mapcat used-symbols (clj-test-files)))
+        untested (remove (comp tested symbol name) testables)]
     (into {} (for [[ns syms] (group-by namespace untested)]
                [ns (map name syms)]))))
 
 (deftask coverage
-  (when-let [needed-additions (not-empty (un-tested-symbols))]
-    (print "The following functions appear to be untested:")
-    (let [width (apply max (map #(.length (key %)) needed-additions))
-          fmt (str "%n  %" width "s:")]
-      (doseq [[ns syms] needed-additions]
-        (print (format fmt ns))
-        (doseq [s syms] (print (str " " s))))
-      (.flush *out*)))) ;; since we're not getting println's auto-flush
+  (let [testables (testable-symbols (testable-namespaces))
+        needed-additions (not-empty (un-tested-symbols testables))
+        untested-count (apply + (map (comp count val) needed-additions))
+        tested-ratio (- 1 (/ untested-count (count testables)))
+        [fmt ratio] (cond
+                     (= 1 tested-ratio)    ["Great!"         1]
+                     (<= 1/2 tested-ratio) ["Almost done..." tested-ratio]
+                     (zero? tested-ratio)  ["Get to work!"   0]
+                     :else                 ["Keep it up!"    (- 1 tested-ratio)])
+        fmt (str "%n%n%2.0f%% of public functions are tested! " fmt)]
+    (when needed-additions
+      (print "The following functions appear to be untested:")
+      (let [width (apply max (map #(.length (key %)) needed-additions))
+            fmt (str "%n  %" width "s:")]
+        (doseq [[ns syms] needed-additions]
+          (print (format fmt ns))
+          (doseq [s syms] (print (str " " s))))))
+    (println (format fmt (* 100.0 ratio)))))
