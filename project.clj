6761d29e602b80d610761fcf1257ba600bda0983
Justin Balthrop <git@justinbalthrop.com>
2011-07-09 22:38:45 -0700

    cleanup and move more stuff to portal. totally broken now

--- project.clj
+++ project.clj
@@ -4,6 +4,7 @@
                  [clojure-contrib "1.2.0"]
                  [uncle "0.2.3"]
                  [depot "0.1.3"]
+                 [portal "0.0.1"]
                  [classlojure "0.5.2"]
                  [useful "0.4.0"]
                  [org.clojure/tools.namespace "0.1.1"]
