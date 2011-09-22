0d7fea95f9c3140a11896d451171072545c93be6
Justin Balthrop <git@justinbalthrop.com>
2011-09-21 22:39:57 -0700

    bump classlojure version

--- project.clj
+++ project.clj
@@ -4,7 +4,7 @@
                  [clojure-contrib "1.2.0"]
                  [uncle "0.2.3"]
                  [depot "0.1.7"]
-                 [classlojure "0.6.0"]
+                 [classlojure "0.6.1"]
                  [useful "0.7.0-alpha3"]
                  [org.clojure/tools.namespace "0.1.1" :exclusions [org.clojure/java.classpath]]
                  [org.clojars.ninjudd/java.classpath "0.1.2-SNAPSHOT"]
