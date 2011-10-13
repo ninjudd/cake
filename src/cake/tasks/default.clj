87a7a67e4b7d9c45518c1d9ba1755c3ea9d86819
Anthony Grimes <i@raynes.me>
2011-10-13 16:26:10 -0500

    Move slamhound out into a plugin.

--- src/cake/tasks/default.clj
+++ src/cake/tasks/default.clj
@@ -2,6 +2,6 @@
   (:use cake.core))
 
 (require-tasks [cake.tasks help jar test ng compile deps release swank file
-                version eval check clojars docs classpath coverage slamhound])
+                version eval check clojars docs classpath coverage])
 
 (deftask default #{help})
--- src/cake/tasks/slamhound.clj
+++
@@ -1,9 +0,0 @@
-(ns cake.tasks.slamhound
-  (:use cake cake.core slam.hound))
-
-(deftask slamhound
-  "Rips your ns form apart and reconstructs it."
-  "Pass a file and slamhound will read your ns form and your code and
-  try to determine what the ns form should look like."
-  {[filename] :slamhound}
-  (println (reconstruct filename)))
