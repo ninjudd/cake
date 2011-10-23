7c9bd13b8b8fc4ec78315cce2a5d4632a24b5bad
Justin Balthrop <git@justinbalthrop.com>
2011-10-23 12:22:55 -0700

    add classpath task to global project

--- src/cake/tasks/global.clj
+++ src/cake/tasks/global.clj
@@ -1,6 +1,6 @@
 (ns cake.tasks.global
   (:use cake.core))
 
-(require-tasks [cake.tasks help deps compile swank file version new eval])
+(require-tasks [cake.tasks help deps compile swank file version new eval classpath])
 
 (deftask default #{help})
