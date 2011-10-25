97b18283a2ebdfd5e08c2c33d5b50cc265b2fb98
Anthony Grimes <i@raynes.me>
2011-10-25 01:46:07 -0500

    Added a clojure-jack-in interface.

--- src/cake/tasks/swank.clj
+++ src/cake/tasks/swank.clj
@@ -3,7 +3,10 @@
         [cake.utils :only [keepalive!]]
         [useful.utils :only [if-ns]]
         [bake.core :only [current-context]])
-  (:import [java.io StringWriter PrintWriter]))
+  (:require [clojure.java.io :as io]
+            [clojure.string :as string])
+  (:import [java.io StringWriter PrintWriter]
+           java.util.jar.JarFile))
 
 (def current-port (atom nil))
 
@@ -48,3 +51,65 @@
   (if @current-port
     (println "swank currently running on port" @current-port)
     (start-swank (or host "localhost:4005"))))
+
+;; # jack-in
+
+(defn elisp-payload-files []
+  (->> (bake [] (.getResources (.getContextClassLoader (Thread/currentThread)) "swank_elisp_payloads.clj"))
+       enumeration-seq
+       (map (comp read-string slurp))
+       (apply concat)
+       set))
+
+(defn hex-digest [file]
+  (bake
+   (:require [clojure.java.io :as io])
+   (:import java.security.MessageDigest)
+   [file file]
+   (format "%x" (BigInteger. 1 (.digest (MessageDigest/getInstance "SHA1")
+                                        (-> file io/resource slurp .getBytes))))))
+
+(defn loader [resource]
+  (let [feature (second (re-find #".*/(.*?).el$" resource))
+        checksum (subs (hex-digest resource) 0 8)
+        basename (format "%s/.emacs.d/swank/%s-%s"
+                         (System/getProperty "user.home")
+                         feature checksum)
+        elisp (str basename ".el")
+        bytecode (str basename ".elc")
+        elisp-file (io/file elisp)]
+    (when-not (.exists elisp-file)
+      (.mkdirs (.getParentFile elisp-file))
+      (with-open [r (.openStream (io/resource resource))]
+        (io/copy r elisp-file))
+      (with-open [w (io/writer elisp-file :append true)]
+        (.write w (format "\n(provide '%s-%s)\n" feature checksum))))
+    (format "(when (not (featurep '%s-%s))
+               (if (file-readable-p \"%s\")
+                 (load-file \"%s\")
+               (byte-compile-file \"%s\" t)))"
+            feature checksum bytecode bytecode elisp)))
+
+(defn payload-loaders []
+  (for [file (elisp-payload-files)]
+    (loader file)))
+
+(deftask jack-in
+ "Jack in to a Clojure SLIME session from Emacs."
+ "This task is intended to be launched from Emacs using M-x clojure-jack-in,
+  which is part of the clojure-mode library."
+ {[port] :jack-in}
+ (println ";;; Bootstrapping bundled version of SLIME; please wait...\n\n")
+ (println (string/join "\n" (payload-loaders)))
+ (println "(sleep-for 0.1)") ; TODO: remove
+ (println "(run-hooks 'slime-load-hook) ; on port" port)
+ (bake (:require swank.swank swank.commands.basic)
+       [port port]
+       (swank.swank/start-server :host "localhost"
+                                 :port (if (and port (string? port))
+                                         (Integer/parseInt port)
+                                         4005)
+                                 :repl-out-root true
+                                 :block true
+                                 :message "\";;; proceed to jack in\"")))
+
