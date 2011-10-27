8455a7263fa8feb92a1e5a28a3b6286a32ba3167
Anthony Grimes <i@raynes.me>
2011-10-27 16:56:18 -0500

    Add update-index and latest-version tasks.

--- src/cake/tasks/search.clj
+++ src/cake/tasks/search.clj
@@ -5,16 +5,28 @@
         [clojure.string :only [join]])
   (:require [sherlock.core :as sherlock]))
 
-(defn- print-results [id page results]
+(defn select-repos [repos]
+  (let [repositories (:repositories *project*)]
+    (if (seq repos)
+      (select-keys repositories repos)
+      repositories)))
+
+(defn download-index [id url]
+  (when (sherlock/download-needed? url)
+    (log (str "Downloading index for " id ". This might take a bit."))
+    (sherlock/update-index url)))
+
+(defn identifier [{:keys [group-id artifact-id]}]
+  (if group-id
+    (str group-id "/" artifact-id)
+    artifact-id))
+
+(defn print-results [id page results]
   (when (seq results)
     (println " == Results from" id "-" "Showing page" page "/"
              (-> results meta :_total-hits (/ sherlock/*page-size*) Math/ceil int) "total")
-    (doseq [{:keys [group-id artifact-id version description]} results]
-      (println (str "[" (if group-id
-                          (str group-id "/" artifact-id)
-                          artifact-id)
-                    " \"" version "\"]")
-               description))
+    (doseq [{:keys [version description] :as artifact} results]
+      (println (str "[" (identifier artifact) " \"" version "\"]") description))
     (prn)))
 
 (deftask search
@@ -24,18 +36,42 @@
    The first time you run this task will take a bit as it has to fetch the
    lucene indexes for your maven repositories.
    Options:
-     --update -- Redownload (update) your existing lucene indexes.
      --page   -- The page number to fetch (each page is 10 results long).
      --repos  -- ids of repositories to search delimited by commas."
-  {update :update [page] :page repos :repos search :search}
-  (let [repositories (:repositories *project*)
-        repos (if repos (select-keys repositories repos) repositories)
-        page (if page (Integer. page) 1)]
+  {[page] :page repos :repos search :search}
+  (let [page (if page (Integer. page) 1)]
     (binding [sherlock/*page-size* 10]
-      (doseq [[id url] repos]
-        (when (or update (sherlock/download-needed? url))
-          (log "Downloading index for" id)
-          (sherlock/update-index url))
+      (doseq [[id url] (select-repos repos)]
+        (download-index id url)
         (print-results
          id page
          (sherlock/get-page page (sherlock/search url (join " " search) page)))))))
+
+(deftask update-repos
+  "Update maven lucene indexes."
+  "If passed arguments, they are treated as the name of maven repos to update.
+   Otherwise, update all repos."
+  {:keys [update-repos]}
+  (doseq [[id url] (select-repos update-repos)]
+    (log (str "Updating index for " id ". This might take a bit."))
+    (sherlock/update-index url)))
+
+(deftask latest-version
+  "Find the latest version of an artifact."
+  "Pass the name of a dependency and cake will search the lucene indexes for it
+   and return the latest version. You can pass --repos which is expected to be a
+   comma delimited list of repository names. If it is not passed, all repositories
+   will be searched."
+  {[term] :latest-version, repos :repos}
+  (let [repos (select-repos repos)
+        [group-id artifact-id] (.split term "/")
+        query (if artifact-id (str "g:" group-id "* AND" artifact-id) group-id)]
+    (doseq [[id url] repos] (download-index id url))
+    (let [result (first (sort-by :version (comp unchecked-negate compare)
+                                 (filter (comp #{term} identifier)
+                                         (mapcat #(sherlock/search % query 10 Integer/MAX_VALUE)
+                                                 (vals repos)))))]
+      (println
+       (if result
+         (str "[" (identifier result) " " (:version result) "]")
+         (println "Nothing was found."))))))
